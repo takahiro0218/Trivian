2f33a55bc75cb5a49e7151cefa2d316e426c2757
EC2 Default User <ec2-user@ip-172-31-2-112.ap-northeast-1.compute.internal>
2023-09-12 00:07:51 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,44 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/edit'
-  end
-  namespace :admin do
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/check'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/show'
-  end
-  namespace :public do
-    get 'post_liks/index'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-# 顧客用
-# URL /users/sign_in ...
-devise_for :users, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] , controllers: {
-  sessions: "admin/sessions"
-}
+  
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] , controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  # 顧客側ルーティング
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :users, only: [:show, :edit, :update, :destroy] do
+      collection do
+        get "check"
+      end
+    resources :posts, only: [:new, :show, :create, :destroy]
+    resources :post_likes, only: [:index, :create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+    end
+  end
+  
+  # 管理者側ルーティング
+  namespace :admin do
+    root to: "homes#top"
+    resources :users, only: [:show, :edit, :update] do
+      member do
+        patch "ban"
+      end
+    end
+    resources :posts, only: [:index, :show, :destory]
+    resources :categories, only: [:index, :create, :edit, :update]
+  end
+  
 end
