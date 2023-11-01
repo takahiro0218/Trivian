0db20211db287979d1202c3f93d1eb6d2abe0e46
takahiro0218 <ashitaka1259@gmail.com>
2023-11-01 16:26:33 +0900

    public login

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,6 +9,9 @@ class Public::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def check
@@ -17,7 +20,7 @@ class Public::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user = current_user
+    if @user == current_user
       if @user.valid_password?(params[:user][:current_password])
         if @user.update(user_params)
           redirect_to user_path(@user.id)
@@ -30,6 +33,7 @@ class Public::UsersController < ApplicationController
         render :edit
       end
     else
+      flash[:notice] = "現在ログイン中のアカウントではないため変更はできません"
       render :edit
     end
   end
