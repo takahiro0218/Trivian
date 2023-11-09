2e2c95c0e256e4043df02a2909dffcb230ed92a7
takahiro0218 <ashitaka1259@gmail.com>
2023-11-09 23:54:59 +0900

    非同期（コメント）途中

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,10 +6,10 @@ class Public::PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = @post.id
     if comment.save
-      redirect_to request.referer
+      # redirect_to request.referer
       flash[:notice] = "コメントを投稿しました"
     else
-      redirect_to request.referer
+      # redirect_to request.referer
       flash[:notice] = "コメントを入力してください"
     end
   end
