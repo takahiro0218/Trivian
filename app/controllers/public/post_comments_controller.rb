60eee6db3e527f93d103c88154e0cba226c965f6
takahiro0218 <ashitaka1259@gmail.com>
2023-11-15 14:54:43 +0900

    非同期（コメント）

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,6 +3,7 @@ class Public::PostCommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @post_comment = PostComment.new
+    @post_comments = @post.post_comments.page(params[:page])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = @post.id
     if comment.save
@@ -16,8 +17,10 @@ class Public::PostCommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
+    @post_comment = PostComment.new
+    @post_comments = @post.post_comments.page(params[:page])
     PostComment.find(params[:id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
     flash[:notice] = "コメントを削除しました"
   end
 
